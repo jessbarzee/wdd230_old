4438b55f97b94a7ceba7e32e684965ad433e8b01
jessbarzee <bar19007@byui.edu>
2022-04-29 21:14:53 -0600

    script attempts

--- js/getdate.js
+++ js/getdate.js
@@ -1,13 +1,16 @@
-try {
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "numeric",
-        year: "numeric"
-    };
-    document.getElementById(
-        "currentdate"
-    ).textContent = new Date().toLocaleDateString("en-US", options);
-} catch(e) {
-    alert("Error with code or your browser does not support Locale");
-}
+// try {
+//     let options = {
+//         weekday: "long",
+//         day: "numeric",
+//         month: "numeric",
+//         year: "numeric"
+//     };
+//     document.getElementById(
+//         "currentdate"
+//     ).textContent = new Date().toLocaleDateString("en-US", options);
+// } catch(e) {
+//     alert("Error with code or your browser does not support Locale");
+// }
+
+const x = document.lastModified;
+document.getElementById("lastupdated").innerHTML = x;
